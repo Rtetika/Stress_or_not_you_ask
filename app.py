45f485b019332210f0e0265e672eeea93ef3cf35
rt etika <64984176+Rtetika@users.noreply.github.com>
2024-05-22 02:23:09 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,19 @@
 import streamlit as st
 import joblib
 
-# Function to load CSS
-def load_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-# Load the custom CSS
-load_css("styles.css")
-
 # Load the model and CountVectorizer
 model = joblib.load('stress_model.sav')
 cv = joblib.load('count_vectorizer.sav')
 
 # Define the Streamlit app
-st.title("Stress Detection")
-st.write("Tell us your situation, what you are feeling? Any symptoms you would like to share? We are here for you.")
+st.title("Stress Detection App")
+st.write("Enter a text to classify it as Stressed or Not Stressed")
 
 # Text input
 user_input = st.text_area("Enter text")
 
 # Predict and display result
-if st.button("Result"):
+if st.button("Predict"):
     if user_input:
         data = cv.transform([user_input]).toarray()
         prediction = model.predict(data)
